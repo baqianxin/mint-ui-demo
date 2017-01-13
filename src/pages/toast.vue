79abffd9ab1e3e00386c3391744db6cf9afba29c
oom-cc <baqianxin@163.com>
2017-01-14 01:27:28 +0800

    修改router

---
+++ src/pages/toast.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="page-toast">
+    <h1 class="page-title">Toast</h1>
+    <div class="page-toast-wrapper">
+      <mt-button @click.native="openToast" size="large">点击弹出 Toast</mt-button>
+      <mt-button @click.native="openToastWithIcon" size="large">点击弹出带有 icon 的 Toast</mt-button>
+      <mt-button @click.native="openBottomToast" size="large">自定义 Toast 位置</mt-button>
+    </div>
+  </div>
+</template>
+
+<script type="text/babel">
+  import { Toast } from 'mint-ui';
+
+  export default {
+    methods: {
+      openToast() {
+        Toast('提示信息');
+      },
+
+      openToastWithIcon() {
+        Toast({
+          message: '操作成功',
+          iconClass: 'mintui mintui-success'
+        });
+      },
+
+      openBottomToast() {
+        Toast({
+          message: '提示信息',
+          position: 'bottom'
+        });
+      }
+    }
+  };
+</script>
