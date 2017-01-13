e90bcde9298d679ff5739f4483d79a613b36df54
oom-cc <baqianxin@163.com>
2017-01-13 21:26:15 +0800

    修改引入模式

--- src/main.js
+++ src/main.js
@@ -4,15 +4,46 @@ import MintUI from 'mint-ui';
 import 'mint-ui/lib/style.css';
 import Vue from 'vue';
 import App from './App';
-import router from './router';
-// 引入并使用vue-resource网络请求模块
+import routes from './route';
 // 引入并使用vue-resource网络请求模块
 import VueResource from 'vue-resource';
-Vue.use(VueResource);
+import VueRouter from 'vue-router';
+
+document.addEventListener('DOMContentLoaded', function() {
+  if (window.FastClick) window.FastClick.attach(document.body);
+}, false);
+
 Vue.use(MintUI);
-var vue = new Vue({
-  el: '#app',
-  router,
-  render: h => h(App)
+Vue.use(VueResource);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  base: __dirname,
+  history: true,
+  routes
 });
-console.log(vue);
+
+new Vue({ // eslint-disable-line
+  el: '#app',
+  render: h => h(App),
+  router
+});
+
+let indexScrollTop = 0;
+router.beforeEach((route, redirect, next) => {
+  if (route.path !== '/') {
+    indexScrollTop = document.body.scrollTop;
+  }
+  document.title = route.meta.title || document.title;
+  next();
+});
+
+router.afterEach(route => {
+  if (route.path !== '/') {
+    document.body.scrollTop = 0;
+  } else {
+    Vue.nextTick(() => {
+      document.body.scrollTop = indexScrollTop;
+    });
+  }
+});
