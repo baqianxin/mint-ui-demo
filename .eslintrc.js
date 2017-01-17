7d9b4af8c7ab1730eae307e9d22554851609d0d9
baqianxin <baqianxin@readtv.cn>
2017-01-17 15:39:39 +0800

    修改 eslint 语法规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,17 +10,17 @@ module.exports = {
   plugins: [
     'html'
   ],
-  // add your custom rules here
+  // custom rules
   'rules': {
-    "quotes": ["off", "double", { "avoidEscape": true }],//双引号定义字符串,允许模板字符串
-    "semi-spacing": [0, {"before": false, "after": true}],//分号前后空格
-    "sort-vars": 0,//变量声明时排序
-    "space-after-keywords": [0, "always"],//关键字后面是否要空一格
-    "space-before-blocks": [0, "always"],//不以新行开始的块{前面要不要有空格
-    "space-before-function-paren": [0, "always"],//函数定义时括号前面要不要有空格
-    "space-in-parens": [0, "never"],//小括号里面要不要有空格
-    "space-infix-ops": 0,//中缀操作符周围要不要有空格
-    "semi": [2, "always"],//语句强制分号结尾
+    "quotes": ["off", "double", { "avoidEscape": true }],// 双引号定义字符串,允许模板字符串
+    "semi-spacing": [0, {"before": false, "after": true}],// 分号前后空格
+    "sort-vars": 0,// 变量声明时排序
+    "space-after-keywords": [0, "always"],// 关键字后面是否要空一格
+    "space-before-blocks": [0, "always"],// 不以新行开始的块{前面要不要有空格
+    "space-before-function-paren": [0, "always"],// 函数定义时括号前面要不要有空格
+    "space-in-parens": [0, "never"],// 小括号里面要不要有空格
+    "space-infix-ops": 0,// 中缀操作符周围要不要有空格
+    "semi": [2, "always"],// 语句强制分号结尾
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
